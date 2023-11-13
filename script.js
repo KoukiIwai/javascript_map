7e6778ad0d5e78888f32e5abcfdd4f82f2148ec1
KoukiIwai <82437777+KoukiIwai@users.noreply.github.com>
2023-11-13 23:50:41 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+// JavaScript
+
+var map = L.map('map').setView([33.67345752177457, 130.44140661378802], 15);
+
+// L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+//     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+// }).addTo(map);
+
+// L.tileLayer('https://cyberjapandata.gsi.go.jp/xyz/std/{z}/{x}/{y}.png', {attribution: '<a href="https://maps.gsi.go.jp/development/ichiran.html" target="_blank">国土地理院</a>',}).addTo(map);
+
+// Open Street Map hot
+L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Tiles style by Humanitarian OpenStreetMap Team hosted by OpenStreetMap France'
+}).addTo(map);
+
+//アイコン
+// const whiteIcon = L.icon({
+//   iconUrl: 'images/ico.png',
+//   shadowUrl: 'images/ico_shadow.png',
+// 
+// iconSize:     [40, 40], // size of the icon
+// shadowSize:   [40, 40], // size of the shadow
+// iconAnchor:   [20, 40], // point of the icon which will correspond to marker's location
+// shadowAnchor: [20, 40],  // the same for the shadow
+// popupAnchor:  [0, -42] // point from which the popup should open relative to the iconAnchor
+// });
+
+//複数アイコンをまとめて定義
+const circleIcon = L.Icon.extend({
+  options: {
+    shadowUrl: 'images/ico_shadow.png',
+    iconSize: [40, 40],
+    shadowSize: [40, 40],
+    iconAnchor: [20, 40],
+    shadowAnchor: [20, 40],
+    popupAnchor: [0, -42]
+  }
+});
+
+const whiteIcon = new circleIcon({ iconUrl: 'images/ico.png' }),
+  pinkIcon = new circleIcon({ iconUrl: 'images/ico_pink.png' });
+
+L.marker([33.67345752177457, 130.44140661378802], { icon: whiteIcon }).addTo(map).bindPopup('こんにちは！<br><img src="images/img01.png" alt="img">');
