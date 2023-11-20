5b0a2632914b623ad825acd87d7e61ea85a06bc7
KoukiIwai <82437777+KoukiIwai@users.noreply.github.com>
2023-11-20 23:00:05 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
 // const whiteIcon = L.icon({
 //   iconUrl: 'images/ico.png',
 //   shadowUrl: 'images/ico_shadow.png',
-// 
+
 // iconSize:     [40, 40], // size of the icon
 // shadowSize:   [40, 40], // size of the shadow
 // iconAnchor:   [20, 40], // point of the icon which will correspond to marker's location
@@ -38,6 +38,41 @@ const circleIcon = L.Icon.extend({
 });
 
 const whiteIcon = new circleIcon({ iconUrl: 'images/ico.png' }),
-  pinkIcon = new circleIcon({ iconUrl: 'images/ico_pink.png' });
+  pinkIcon = new circleIcon({ iconUrl: 'images/ico_pink.png' }),
+  blueIcon = new circleIcon({ iconUrl: 'images/ico_blue.png' });
 
 L.marker([33.67345752177457, 130.44140661378802], { icon: whiteIcon }).addTo(map).bindPopup('こんにちは！<br><img src="images/img01.png" alt="img">');
+L.marker([33.65958150849491, 130.4440143454105], { icon: pinkIcon }).addTo(map).bindPopup('こんにちは！');
+L.marker([33.67020170623659, 130.4345714724636], { icon: blueIcon }).addTo(map).bindPopup('こんにちは！');
+
+const circle = L.circle([33.67345752177457, 130.44140661378802], {
+  color: 'red', //円の輪郭線の色
+  fillColor: '#f03', //円の塗りつぶしの色
+  fillOpacity: 0.3, //塗りつぶしの不透明度
+  radius: 1000 //半径、メートルで指定
+}).addTo(map);
+
+circle.bindPopup("半径1kmの範囲");
+
+// 多角形の表示
+const polygon = L.polygon([
+  [33.654566, 130.442605],
+  [33.649279, 130.440245],
+  [33.650137, 130.452476]
+], {
+  color: 'blue',
+  fillColor: '#30f',
+  fillOpacity: 0.3
+}).addTo(map);
+
+// クリック位置の緯度経度表示
+const popup = L.popup();
+
+function onMapClick(e) {
+  popup
+    .setLatLng(e.latlng)
+    .setContent("ここは" + e.latlng.toString() + "です")
+    .openOn(map);
+}
+
+map.on('click', onMapClick);
